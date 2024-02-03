c22f93d05750bb5d39233be74ea5c05b21e5cec6
Darshan Prabhudas Panchal <91986412+darshan472002@users.noreply.github.com>
2024-02-03 14:30:06 +0530

    Upload the all project files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
 
+// INTERNAL IMPORT
+import { TrackingProvider } from '@/Conetxt/TrackingContext';
+import { NavBar, Footer } from '@/Components';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TrackingProvider>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </TrackingProvider>
+    </>
+  );
 }
